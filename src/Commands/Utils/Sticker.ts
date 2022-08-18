bcae5dc3b3b35e69f64c7e3fa7588dd1a5b4c91f
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-08-19 03:06:15 +0530

    Update Sticker.ts

--- src/Commands/Utils/Sticker.ts
+++ src/Commands/Utils/Sticker.ts
@@ -29,7 +29,7 @@ export default class extends BaseCommand {
         const sticker = new Sticker(buffer, {
             categories,
             pack: pack[1] ? pack[1].trim() : '🤍 Made for you',
-            author: pack[2] ? pack[2].trim() : `${this.client.config.name} 🖤`,
+            author: pack[2] ? pack[2].trim() : `ᥴꫝꫀꪗ-𝙎𝙖𝙣💫🌙🪐✨🖤`,
             quality,
             type:
                 flags.includes('--c') || flags.includes('--crop') || flags.includes('--cropped')
