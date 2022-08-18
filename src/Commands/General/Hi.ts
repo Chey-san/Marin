ff44352b038b5fcba13bbeb8d3a3bd5a911cd2fb
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-08-19 01:39:39 +0530

    Update Hi.ts

--- src/Commands/General/Hi.ts
+++ src/Commands/General/Hi.ts
@@ -9,6 +9,17 @@ import { BaseCommand, Command, Message } from '../../Structures'
     cooldown: 5
 })
 export default class extends BaseCommand {
-    public override execute = async ({ sender, reply }: Message): Promise<void> =>
-        void (await reply(`Hello! *${sender.username}*`))
-}
+    public override execute = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			'https://telegra.ph/file/7085dedc2f8a5165cd602.mp4'
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: `I don't have time to have a conversation with someone like you. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
+			}
+		);
+	};
